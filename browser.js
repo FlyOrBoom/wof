c1abfa79d65b929d00dd993cf803e92f4bb7b705
XYZt <31298876+FlyOrBoom@users.noreply.github.com>
2019-08-31 15:08:07 -0700

    Add files via upload

--- browser.js
+++ browser.js
@@ -10,10 +10,9 @@ var omnibox = document.getElementById("omnibox"),
     opened = [0],
     topchrome = document.getElementById("topchrome"),
     highlight = document.getElementById('topchrome-highlight'),
+    textlight = document.getElementById('topchrome-textlight'),
     toolbar = document.getElementById('topchrome-tools'),
-    iframe = document.getElementById('iframe'),
     closeWindow = document.getElementById('close');
-
 
 function setCurrent(k){
   console.log('function setCurrent('+k+')');
@@ -21,7 +20,8 @@ function setCurrent(k){
   webview[k].classList='current-webview';
   tab[k].classList='current-tab';
   current=k;
-  omnibox.value='';
+  omnibox.value=webview[current].src;
+  checkHome();
   if(tab.length>1){
     for(var i = 0;i<tab.length;i++){
       if(i!==k){
@@ -35,64 +35,98 @@ function setCurrent(k){
 
 
 function newTab(url){
+  
   console.log('function newTab('+url+')');
+  
   var j = tab.length;
   
   webview[j] = document.createElement('webview');
+  // webview[j].setAttribute('partition',('trusted-'+(Math.random()*10)));
+  webview[j].setAttribute('partition','trusted');
   webview[j].id = 'webview'+j;
-  webview[j].src = url;
   webviewContainer.appendChild(webview[j]);
-  
+
   tab[j] = document.createElement('div');
   tab[j].id = 'tab'+j;
   tabContainer.appendChild(tab[j]);
-  
   setCurrent(j);
-  tab[j].addEventListener('click',function(e){setCurrent(j)});
-  
-  webview[j].onmouseover = function(){
-    if(webview[j].src){
-      webview[j].focus();
+  tab[j].addEventListener('click',function(e){
+    if(j===current){
+      if(tab.length>1){
+        webview[j].remove();
+        tab[j].remove();
+        webview.splice(j);
+        tab.splice(j);
+        setCurrent(j-1);
+      }else{
+        window.close();
+      }
+    }else{
+      setCurrent(j);
     }
+  });
+  
+  webview[j].onmousemove = function(){
+    webview[j].focus();
   };
   webview[j].addEventListener('loadstart',function(e){
-    omnibox.value=e.url;
+    if(e.isTopLevel){
+      omnibox.value=e.url;
+      checkHome();
+    }
   });
   webview[j].addEventListener('loadstop',function(e){
-    omnibox.value=webview[j].src;
+    omnibox.value=webview[current].src;
+    checkHome();
   });
   webview[j].addEventListener("permissionrequest", function(e) {
     e.request.allow();
   });
   webview[j].addEventListener("newwindow", function(e) {
-    newTab(e.targetUrl);
+    newTab();
+    webview[current].src=e.targetUrl;
   });
-  iframe.style.display='block';
+  
+  setTimeout(function(){webview[0].src=url;},1);
 }
 
+function checkHome(){
+  if(omnibox.value.startsWith('chrome-extension')||omnibox.value=='offline/home.html'){
+    omnibox.value='';
+  }
+}
 
-newTab();
+newTab('offline/home.html');
+checkHome();
 omnibox.focus();
 
-tabNew.onclick = function(){newTab()};
+omnibox.onclick = function(){
+  omnibox.select();
+};
+tabNew.onclick = function(){newTab();webview[current].src='offline/home.html'};
 omnibox.onclick = function(){
   omnibox.select();
 };
 search.onsubmit = function() {
   var e = omnibox.value;
-  if(!(e.includes("."))){
-    e = "duckduckgo.com/" + e + "&kp=-2";
-  }
-  if(!(e.includes("http"))){
-    e = "https://" + e;
+  if(e){
+    if(!(e.includes("."))){
+      e = "duckduckgo.com/" + e + "&kp=-2";
+    }
+    if(!(e.includes("http"))){
+      e = "https://" + e;
+    }
+    webview[current].src=e;
+  }else{
+    webview[current].src='offline/home.html';
+    e='';
   }
   omnibox.value=e;
-  webview[current].src=e;
-  iframe.style.display='none';
 };
 document.onmousemove = function (e){
   highlight.style.transform = 'translate('+e.pageX+'px,0)';
-  toolbar.style.webkitMaskPositionX = (e.pageX-window.innerWidth)+'px';
+  textlight.style.transform = 'translate('+e.pageX+'px,0)';
+  omnibox.focus();
 };
 closeWindow.onclick = function(){
   window.close();
