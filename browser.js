09242afc20d6f0e61643fd2eea9edd83ba195ad7
XYZt <31298876+FlyOrBoom@users.noreply.github.com>
2019-09-09 20:51:36 -0700

    glass ascetic

--- browser.js
+++ browser.js
@@ -5,63 +5,65 @@ var doc = document,
     navForward = doc.getElementById("nav-forward"),
     webviewContainer = doc.getElementById('webview-container'),
     webview = [],
+    webviewhold = [],
     tabContainer = doc.getElementById('tab-container'),
     tab = [],
+    tabhold = [], //temp storage for rearranging tabs
     tabNew = doc.getElementById('tab-new'),
     current = 0,
     topchrome = doc.getElementById("topchrome"),
     closeWindow = doc.getElementById('close'),
+    miniWindow = doc.getElementById('mini'),
     snackbar = doc.getElementById('snackbar'),
     tick = {invert:0,fog:0},
+    rem = parseFloat(getComputedStyle(doc.documentElement).fontSize),
     root = doc.documentElement;
 
 
 function setCurrent(k){
   console.log('function setCurrent('+k+')');
   console.log('Set #webview'+k+' as current tab');
-  webview[k].classList='current-webview';
-  tab[k].classList.add('current');
   current=k;
+  webview[current].classList='current-webview';
+  tab[current].classList.add('current');
   omnibox.value=webview[current].src;
   checkHome();
   if(tab.length>1){
     for(let i=0;i<tab.length;i++){
-      if(i!==k){
-        webview[i].classList = '';
+      if(i!==current){
+        webview[i].classList.remove('current-webview');
         tab[i].classList.remove('current');
-        console.log('Deactivated #webview'+i+' and #tab'+i);
       }
     }
   }
 }
 
-function omniUrl(){
-  omnibox.value=webview[current].src;
+function omniUrl(inputSrc){
+  omnibox.value=inputSrc;
   checkHome();
 }
 
-function closeTab(){
-  console.log('function CloseTab('+current+')');
+function closeTab(input){
+  console.log('function CloseTab('+input+')');
   if(webviewContainer.childElementCount>1){
-    tab[current].classList.add('close');
+    tab[input].classList.add('close');
     setTimeout(function(){
-      webview[current].remove();
-      tab[current].remove();
-      tab[current]=false;
-      webview[current]=false;
-      while(!(webview[current])){
-        console.log(current);
-        if(current<1){
-          current+=tab.length;
-        }else{
-          current--;
-        }
+      webview[input].remove();
+      webview.splice(input,1);
+      tab[input].remove();
+      tab.splice(input,1);
+      if(!(webview[current])){
+        current--;
       }
       setCurrent(current);
     },100);
   }else{
     window.close();
   }
+}
+
+function matchUrl(url){
+  return url.match(/(?<=:\/\/)(.*)(?=\.)/g)[0].replace(/(www\.)/,'');
 }
 
 function newTab(url){
@@ -73,39 +75,46 @@ function newTab(url){
   webview[j] = doc.createElement('webview');
   // webview[j].setAttribute('partition',('trusted-'+(Math.random()*10)));
   webview[j].setAttribute('partition','trusted');
-  webview[j].id = 'webview'+j;
   webview[j].currentUrl='';
   webview[j].setAttribute('allowtransparency','on');
   webviewContainer.appendChild(webview[j]);
 
   tab[j] = doc.createElement('div');
-  tab[j].id = 'tab'+j;
-  tab[j].classList.add('tab','button');
+  tab[j].classList.add('tab');
   tab[j].tabindex = 10+j;
   tabContainer.insertAdjacentElement('afterbegin',tab[j]);
   setCurrent(j);
-  tab[j].addEventListener('click',function(){
-    if(j===current){
-      closeTab();
+  tab[j].addEventListener('click',function(e){
+    j=tab.indexOf(this);
+    if(e.offsetX+(1.25*rem)>tab[j].scrollWidth&&e.offsetX<tab[j].scrollWidth){
+      closeTab(j);
     }else{
       setCurrent(j);
     }
   });
   
   webview[j].onmousemove = function(e){
-    webview[j].focus();
+    if(!(snackbar.hasFocus())){
+      this.focus();
+    }
   };
   webview[j].addEventListener('loadstart',function(e){
-    // if(j===current&&e.isTopLevel&&(webview[j].currentUrl.match(/(?<=:\/\/)(.*)(?=\.)/g)[0]!==webview[j].src.match(/(?<=:\/\/)(.*)(?=\.)/g)[0])){
-    //   loadwrapper.classList.add('loading');
-    //   console.log(true);
-    // }
-    omniUrl();
+    j=webview.indexOf(this);
+    if(e.isTopLevel){
+      // loadwrapper.classList.add('loading');
+      tab[j].setAttribute('data-domain',matchUrl(e.url));
+      omnibox.focus();
+      if(j===current){
+        omniUrl(e.url);
+      }
+    }
   });
   webview[j].addEventListener('loadstop',function(){
+    j=webview.indexOf(this);
     this.insertCSS({code:'body::before{background-color:#fff}'});
-    omniUrl();
+    omniUrl(webview[current].src);
     this.currentUrl=this.src;
+    tab[j].setAttribute('data-domain',matchUrl(this.src));
   });
   webview[j].addEventListener("permissionrequest", function(e) {
     e.request.allow();
@@ -174,8 +183,13 @@ search.onsubmit = function() {
 };
 window.onmousemove = function (e){
   topchrome.style.backgroundPosition = (100*(window.innerWidth-e.pageX)/window.innerWidth)+'%';
-  omnibox.focus();
+  if(!(snackbar.hasFocus())){
+    omnibox.focus();
+  }
 };
+miniWindow.onclick = function(){
+  chrome.app.window.current().minimize();
+}
 closeWindow.onclick = function(){
   window.close();
 };
@@ -188,7 +202,7 @@ function(e){
       if(e.ctrlKey){
         clearAllHistory();
       }else{
-       closeTab();
+       closeTab(current);
       }
     }else if(e.key==='n'){
       chrome.app.window.create('browser.html', {
@@ -230,11 +244,13 @@ function(e){
 },false);
 
 setInterval(function checkFullscreen(){
-  if(window.screen.width===window.innerWidth&&window.screen.height===window.innerHeight){
-    topchrome.classList.add('fullscreen');
-    webviewContainer.classList.add('fullscreen');
-  }else{
-    topchrome.classList.remove('fullscreen');
-    webviewContainer.classList.remove('fullscreen');
+  if(topchrome.classList){
+    if(window.screen.width===window.innerWidth&&window.screen.height===window.innerHeight){
+      topchrome.classList.add('fullscreen');
+      webviewContainer.classList.add('fullscreen');
+    }else{
+      topchrome.classList.remove('fullscreen');
+      webviewContainer.classList.remove('fullscreen');
+    }
   }
 },10);
